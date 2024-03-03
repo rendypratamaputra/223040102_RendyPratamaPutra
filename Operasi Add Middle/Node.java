a3c0322ea5923cd77afd70c1a1c5296de51f4fd0
rendypratamaputra <126095967+rendypratamaputra@users.noreply.github.com>
2024-03-03 22:47:49 +0700

    Add files via upload

---
+++ Operasi Add Middle/Node.java
@@ -0,0 +1,23 @@
+package Pertemuan4Perograman1;
+
+public class Node {
+    private double data;
+    private Node next;
+
+    public Node(double data) {
+        this.data = data;
+    }
+
+    // Setter dan Getter
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public double getdata() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+}
